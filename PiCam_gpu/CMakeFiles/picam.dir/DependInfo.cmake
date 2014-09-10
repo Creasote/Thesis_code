19f9a8a741c6eb138adc961398e484694d0fa5bb
creasote <chris@mrcreasote.com>
2014-09-10 16:35:09 +0000

    Working PiCam_gpu install, no other modifications

---
+++ PiCam_gpu/CMakeFiles/picam.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/code/PiCam_gpu/camera.cpp" "/home/pi/code/PiCam_gpu/CMakeFiles/picam.dir/camera.cpp.o"
+  "/home/pi/code/PiCam_gpu/cameracontrol.cpp" "/home/pi/code/PiCam_gpu/CMakeFiles/picam.dir/cameracontrol.cpp.o"
+  "/home/pi/code/PiCam_gpu/graphics.cpp" "/home/pi/code/PiCam_gpu/CMakeFiles/picam.dir/graphics.cpp.o"
+  "/home/pi/code/PiCam_gpu/lodepng.cpp" "/home/pi/code/PiCam_gpu/CMakeFiles/picam.dir/lodepng.cpp.o"
+  "/home/pi/code/PiCam_gpu/picam.cpp" "/home/pi/code/PiCam_gpu/CMakeFiles/picam.dir/picam.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/opt/vc/include"
+  "/opt/vc/userland-master/interface/vcos"
+  "/opt/vc/userland-master"
+  "/opt/vc/userland-master/interface/vcos/pthreads"
+  "/opt/vc/userland-master/interface/vmcs_host/linux"
+  "/opt/vc/userland-master/host_applications/linux/libs/bcm_host/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
