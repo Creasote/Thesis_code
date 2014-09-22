667a924a058b4a7203723b09aaeb640cccf3c4cd
creasote <chris@mrcreasote.com>
2014-09-22 19:45:46 +0000

    Working BRG 15fps 512x512 with 50 sign

--- Thesis/src/main3501.cpp
+++ Thesis/src/main3501.cpp
@@ -7,29 +7,35 @@
 #include <sys/time.h>
 #include <fstream>
 
-# define WIDTH 512
-# define HEIGHT 512
+//#include "PiCapture.h"
+//#include <fcntl.h>
+
+# define WIDTH 1024
+# define HEIGHT 1024
 
 using namespace cv;
 using namespace std;
 
 
-/*
-	Mat imgRef40;
-	Mat imgRef60;
-	Mat imgRef80;
-	Mat imgRef90;
-	Mat imgRef100;
-	Mat imgRef110;
+
+	Mat imgRefForty;
+	Mat imgRefSixty;
+	Mat imgRefEighty;
+	Mat imgRefNinety;
+	Mat imgRefHundred;
+	Mat imgRefHundredTen;
 	Mat vec40;
-	Mat vec50;
 	Mat vec60;
 	Mat vec80;
 	Mat vec90;
 	Mat vec100;
 	Mat vec110;
-	*/
 
+	int jumpFrameFlag = 0;
+
+void setVideoPositionCallback(int frameNumber, void *userData){
+	jumpFrameFlag = 1;
+}
 
 int main(int argc, char *argv[])
 {
@@ -41,60 +47,54 @@ int main(int argc, char *argv[])
 	int saveFileSuffix = 1;
 	char saveFileName[50];
 	char videoFileName[50];
-	int goalFPS = 15;
+	int goalFPS = 30;
 
 	// Buffer for saving raw stream from camera
 	char imgBufferOrig[WIDTH*HEIGHT*4];
 	char imgBufferScaled[(WIDTH/imgScalingFactor)*(HEIGHT/imgScalingFactor)*4];
 
 
-	//imgRef60Zero = imread("C:\\Thesis\\used\\sixtycropped0.jpg", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef40 = imread("/mnt/Thesis/reference/speed40.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef50 = imread("/mnt/Thesis/reference/speed50.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef60 = imread("/mnt/Thesis/reference/speed60.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef80 = imread("/mnt/Thesis/reference/speed80.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef90 = imread("/mnt/Thesis/reference/speed90.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef100 = imread("/mnt/Thesis/reference/speed100.bmp", CV_LOAD_IMAGE_UNCHANGED);
-	Mat imgRef110 = imread("/mnt/Thesis/reference/speed110.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	//imgRefSixtyZero = imread("C:\\Thesis\\used\\sixtycropped0.jpg", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefForty = imread("/mnt/Thesis/reference/speed40.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefSixty = imread("/mnt/Thesis/reference/speed60.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefEighty = imread("/mnt/Thesis/reference/speed80.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefNinety = imread("/mnt/Thesis/reference/speed90.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefHundred = imread("/mnt/Thesis/reference/speed100.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	imgRefHundredTen = imread("/mnt/Thesis/reference/speed110.bmp", CV_LOAD_IMAGE_UNCHANGED);
+	//resize(imgRefSixtyZero, imgRefSixtyZero, Size(30,30));
 
-	Mat vec40 = imread("/mnt/Thesis/reference/speed40.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec50 = imread("/mnt/Thesis/reference/speed50.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec60 = imread("/mnt/Thesis/reference/speed60.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec80 = imread("/mnt/Thesis/reference/speed80.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec90 = imread("/mnt/Thesis/reference/speed90.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec100 = imread("/mnt/Thesis/reference/speed100.png", CV_LOAD_IMAGE_UNCHANGED);
-	Mat vec110 = imread("/mnt/Thesis/reference/speed110.png", CV_LOAD_IMAGE_UNCHANGED);
-
-	Mat speedDisplay[] = {vec40, vec50, vec60, vec80, vec90, vec100, vec110};
-	int speedsChecked[] = {40,50,60,80,90,100,110};
+	vec40 = imread("/mnt/Thesis/reference/speed40.png", CV_LOAD_IMAGE_UNCHANGED);
+	vec60 = imread("/mnt/Thesis/reference/speed60.png", CV_LOAD_IMAGE_UNCHANGED);
+	vec80 = imread("/mnt/Thesis/reference/speed80.png", CV_LOAD_IMAGE_UNCHANGED);
+	vec90 = imread("/mnt/Thesis/reference/speed90.png", CV_LOAD_IMAGE_UNCHANGED);
+	vec100 = imread("/mnt/Thesis/reference/speed100.png", CV_LOAD_IMAGE_UNCHANGED);
+	vec110 = imread("/mnt/Thesis/reference/speed110.png", CV_LOAD_IMAGE_UNCHANGED);
 
 	if(vec40.empty()){
 		cout<<"Failed to open vector files"<<endl;
 		return 0;
 	}
+	else{
+		//imshow("40 Sign", vec40);
+		cout<<"It appears the vec files loaded... maybe?"<<endl;
+	}
 
 	ofstream logFile ("/mnt/Thesis/used/video/logfile.txt");
 
-	Mat imgHSV((HEIGHT/(imgScalingFactor * 2)),(WIDTH/(imgScalingFactor * 2)), CV_8UC3);
-	Mat imgThreshold1((HEIGHT/(imgScalingFactor * 2)),(WIDTH/(imgScalingFactor * 2)), CV_8UC3);
-	Mat imgThreshold2((HEIGHT/(imgScalingFactor * 2)),(WIDTH/(imgScalingFactor * 2)), CV_8UC3);
-	Mat imgThresholded((HEIGHT/(imgScalingFactor * 2)),(WIDTH/(imgScalingFactor * 2)), CV_8UC3);
+	Mat imgHSV;
+	Mat imgThresholded;
 	Mat imgObjectUnknown;
-	Mat imgFocus1;
-	Mat imgFocus2;
 	Mat imgFocussed;
 	Mat imgObjectConfirmed;
 
 	Mat imgOrigCam(HEIGHT, WIDTH, CV_8UC4, (void *)imgBufferOrig);
-	Mat imgOrigFull(imgOrigCam.rows, imgOrigCam.cols, CV_8UC3);
+	Mat imgOrig(imgOrigCam.rows, imgOrigCam.cols, CV_8UC3);
 	Mat imgOrigAlpha(imgOrigCam.rows, imgOrigCam.cols, CV_8UC1);
-	Mat arrayOrig[] = {imgOrigFull, imgOrigAlpha};
-	Mat imgOrig((HEIGHT/3),(WIDTH/3),CV_8UC3);
+	Mat arrayOrig[] = {imgOrig, imgOrigAlpha};
 	Mat imgScaledCam(HEIGHT/imgScalingFactor, WIDTH/imgScalingFactor, CV_8UC4, (void *)imgBufferScaled);
-	Mat imgScaledFull(imgScaledCam.rows, imgScaledCam.cols, CV_8UC3);
+	Mat imgScaled(imgScaledCam.rows, imgScaledCam.cols, CV_8UC3);
 	Mat imgScaledAlpha(imgScaledCam.rows, imgScaledCam.cols, CV_8UC1);
-	Mat arrayScaled[] = {imgScaledFull, imgScaledAlpha};
-	Mat imgScaled((HEIGHT/(imgScalingFactor * 2)),(WIDTH/(imgScalingFactor * 2)), CV_8UC3);
+	Mat arrayScaled[] = {imgScaled, imgScaledAlpha};
 	int channelList[] = {0,2,1,1,2,0,3,3}; // rearrange so that RGB -> BRG and alpha channel is split off
 
 
@@ -103,7 +103,6 @@ int main(int argc, char *argv[])
 	int frameDisplayCounter = 0;
 
 	Mat confidenceForty(11, 10, CV_32F);
-	Mat confidenceFifty(11, 10, CV_32F);
 	Mat confidenceSixty(11, 10, CV_32F);
 	Mat confidenceEighty(11, 10, CV_32F);
 	Mat confidenceNinety(11, 10, CV_32F);
@@ -113,7 +112,7 @@ int main(int argc, char *argv[])
 
 
 	// Open the camera
-	CCamera* cap = StartCamera(WIDTH, HEIGHT, (goalFPS+5), (downsampleLevels+1), true);
+	CCamera* cap = StartCamera(WIDTH, HEIGHT, goalFPS, (downsampleLevels+1), true);
 
 	/*
 	// Output
@@ -135,12 +134,9 @@ int main(int argc, char *argv[])
 	int val1amin = 0;
 	int val1amax = 20;
 
-
 	// Build menu here
 	createTrackbar("val1min", "Menu", &val1min, 255);
 	createTrackbar("val1max", "Menu", &val1max, 255);
-	createTrackbar("val1amin", "Menu", &val1amin, 255);
-	createTrackbar("val1amax", "Menu", &val1amax, 255);
 	createTrackbar("val2min", "Menu", &val2min, 255);
 	createTrackbar("val2max", "Menu", &val2max, 255);
 	createTrackbar("val3min", "Menu", &val3min, 255);
@@ -155,12 +151,10 @@ int main(int argc, char *argv[])
 		// Capture full-size RGBA reference image and shift to BGR
 		cap->ReadFrame(0, imgBufferOrig, sizeof(imgBufferOrig));
 		mixChannels(&imgOrigCam, 1, arrayOrig, 2, channelList, 4);
-		imgOrig = imgOrigFull(Rect(0,(HEIGHT/3),(WIDTH/2),(HEIGHT/3)));
 
 		// Capture scaled-size RGBA image for processing and shift to BGR
 		cap->ReadFrame(downsampleLevels, imgBufferScaled, sizeof(imgBufferScaled));
 		mixChannels(&imgScaledCam, 1, arrayScaled, 2, channelList, 4);
-		imgScaled = imgScaledFull(Rect(0,(HEIGHT/(3 * imgScalingFactor)),(WIDTH/(2 * imgScalingFactor)),(HEIGHT/(3 * imgScalingFactor))));
 
 		// Ensure frames loaded correctly
 		if (imgOrig.empty()){
@@ -182,9 +176,7 @@ int main(int argc, char *argv[])
 		cvtColor(imgScaled,imgHSV,CV_BGR2HSV);
 
 		// Threshold for colour
-		inRange(imgHSV,Scalar(val1min,val2min,val3min),Scalar(val1max,val2max,val3max),imgThreshold1);
-		inRange(imgHSV,Scalar(val1amin,val2min,val3min),Scalar(val1amax,val2max,val3max),imgThreshold2);
-		bitwise_or(imgThreshold1, imgThreshold2, imgThresholded);
+		inRange(imgHSV,Scalar(val1min,val2min,val3min),Scalar(val1max,val2max,val3max),imgThresholded);
 
 		// remove small artefacts
 		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(erodeSize,erodeSize)));
@@ -194,7 +186,7 @@ int main(int argc, char *argv[])
 		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(dilateSize,dilateSize)));
 		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(erodeSize,erodeSize)));
 
-		//GaussianBlur(imgThresholded, imgThresholded, Size(5,5),2,2);
+		GaussianBlur(imgThresholded, imgThresholded, Size(5,5),2,2);
 		vector<Vec3f> circles;
 		HoughCircles(imgThresholded, circles, CV_HOUGH_GRADIENT,
 			3,	// accumulator resolution (size of image / 3)
@@ -229,23 +221,22 @@ int main(int argc, char *argv[])
 			int signHeightOversize = signHeight * 1.4;
 			int maxXcoord = signPosX + (signWidthOversize);
 			if (maxXcoord > imgOrigSize.width){
-				signPosX = signPosX - (maxXcoord - imgOrigSize.width + 1);
+				signPosX = signPosX - (maxXcoord - imgOrigSize.width +1);
 			}
 			int maxYcoord = signPosY + (signHeightOversize);
 			if (maxYcoord > imgOrigSize.height){
 				signPosY = signPosY - (maxYcoord - imgOrigSize.height + 1);
 			}
 
-			imgObjectUnknown = imgOrig(Rect(signPosX, signPosY, signWidth * 1.4, signHeight * 1.4));
+			imgObjectUnknown = imgOrig(Rect(signPosX, signPosY, signWidth*1.4, signHeight*1.4));
 
 
 			// Convert to HSV
 			cvtColor(imgObjectUnknown,imgFocussed,CV_BGR2HSV);
+			//cvtColor(imgObjectUnknown,imgFocussed,CV_RGB2HSV);
 
 			// Threshold for desired colour
-			inRange(imgFocussed,Scalar(val1min,val2min,val3min),Scalar(val1max,val2max,val3max),imgFocus1);
-			inRange(imgFocussed,Scalar(val1amin,val2min,val3min),Scalar(val1amax,val2max,val3max),imgFocus2);
-			bitwise_or(imgFocus1, imgFocus2, imgFocussed);
+			inRange(imgFocussed,Scalar(val1min,val2min,val3min),Scalar(val1max,val2max,val3max),imgFocussed);
 
 			GaussianBlur(imgFocussed, imgFocussed, Size(5,5),2,2);
 			vector<Vec3f> circlesFocussed;
@@ -259,69 +250,81 @@ int main(int argc, char *argv[])
 			for ( size_t objectCount = 0; objectCount < circlesFocussed.size(); objectCount++){
 				Point centerFocussed(cvRound(circlesFocussed[objectCount][0]), cvRound(circlesFocussed[objectCount][1]));
 				int radiusFocussed = cvRound(circlesFocussed[objectCount][2]);
+				//cout<<"(sub) Centre: "<<centerFocussed<<" radius: "<<radiusFocussed<<endl;
+				//int currentFrame = cap.get(CV_CAP_PROP_POS_FRAMES);
+				//cout<<"Frame: "<<currentFrame<<", sign found at: "<<centerFocussed<<" of radius: "<<radiusFocussed<<endl;
 
+				/*
+				int signPosXFocussed = (circlesFocussed[objectCount][0]-(radius/2));
+				if (signPosXFocussed < 0) signPosXFocussed = 1;
+				int signPosYFocussed = (circlesFocussed[objectCount][1]-(radius/2));
+				if (signPosYFocussed < 0) signPosYFocussed = 1;
+				*/
+
+				//if(waitKey(1000) == 27) break;
 				// Resize image to known size for template matching
 				resize(imgObjectUnknown,imgObjectConfirmed,Size(35,36));  // was size(40,40)
 
-				matchTemplate(imgObjectConfirmed, imgRef40, confidenceForty, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef50, confidenceFifty, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef60, confidenceSixty, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef80, confidenceEighty, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef90, confidenceNinety, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef100, confidenceHundred, CV_TM_CCOEFF_NORMED);
-				matchTemplate(imgObjectConfirmed, imgRef110, confidenceHundredTen, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefForty, confidenceForty, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefSixty, confidenceSixty, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefEighty, confidenceEighty, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefNinety, confidenceNinety, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefHundred, confidenceHundred, CV_TM_CCOEFF_NORMED);
+				matchTemplate(imgObjectConfirmed, imgRefHundredTen, confidenceHundredTen, CV_TM_CCOEFF_NORMED);
 
 				double detectionConfidence = 0;
-				double detectionConfidenceAlt = 0;
-				double confidences[7];
+				double confidences[6];
 
 				minMaxLoc(confidenceForty, NULL, &confidences[0], NULL, NULL, Mat());
-				minMaxLoc(confidenceFifty, NULL, &confidences[1], NULL, NULL, Mat());
-				minMaxLoc(confidenceSixty, NULL, &confidences[2], NULL, NULL, Mat());
-				minMaxLoc(confidenceEighty, NULL, &confidences[3], NULL, NULL, Mat());
-				minMaxLoc(confidenceNinety, NULL, &confidences[4], NULL, NULL, Mat());
-				minMaxLoc(confidenceHundred, NULL, &confidences[5], NULL, NULL, Mat());
-				minMaxLoc(confidenceHundredTen, NULL, &confidences[6], NULL, NULL, Mat());
+				minMaxLoc(confidenceSixty, NULL, &confidences[1], NULL, NULL, Mat());
+				minMaxLoc(confidenceEighty, NULL, &confidences[2], NULL, NULL, Mat());
+				minMaxLoc(confidenceNinety, NULL, &confidences[3], NULL, NULL, Mat());
+				minMaxLoc(confidenceHundred, NULL, &confidences[4], NULL, NULL, Mat());
+				minMaxLoc(confidenceHundredTen, NULL, &confidences[5], NULL, NULL, Mat());
 
 				int speedFlag = 0;
-				int speedFlagAlt = 0;
 				for (int counter = 0; counter < 6; counter++){
 					if (confidences[counter] > detectionConfidence){
-						speedFlagAlt = speedFlag;
-						detectionConfidenceAlt = detectionConfidence;
 						detectionConfidence = confidences[counter];
 						speedFlag = counter;
 					}
 				}
 
 
-				if (detectionConfidence > 0.4)
+				if ((detectionConfidence > 0.4)|| (detectionConfidence < -0.4))
 				{
 					imshow("Detected Object", imgObjectUnknown);
 					objectSignConfirmed = 1;
 
-					/*   ENABLE THIS once shifted vectors are available
-					if ()(detectionConfidence - detectionConfidenceAlt)/detectionConfidence > 0.2){
-						double secondaryConfidences[2];
-
-						switch (speedFlag)
-						{
-							case 0:
-							// recheck 40;
-							break;
-							case 1:
-							// recheck 60
-							break;
-							default:
-							break;
-						}
-
+					switch (speedFlag)
+					{
+					case 0:
+						imshow("Speed Zone", vec40);
+						currentSpeed = 40;
+						break;
+					case 1:
+						imshow("Speed Zone", vec60);
+						currentSpeed = 60;
+						break;
+					case 2:
+						imshow("Speed Zone", vec80);
+						currentSpeed = 80;
+						break;
+					case 3:
+						imshow("Speed Zone", vec90);
+						currentSpeed = 90;
+						break;
+					case 4:
+						imshow("Speed Zone", vec100);
+						currentSpeed = 100;
+						break;
+					case 5:
+						imshow("Speed Zone", vec110);
+						currentSpeed = 110;
+						break;
+					default:
+						break;
 					}
-					*/
-
-					imshow("Speed Zone", speedDisplay[speedFlag]);
-					currentSpeed = speedsChecked[speedFlag];
-
 					sprintf(saveFileName, "/mnt/Thesis/extracts/positive%03d-%04d.jpg", currentSpeed, saveFileSuffix); // mnt/Thesis/extracts/
 					imwrite(saveFileName, imgObjectUnknown);
 					saveFileSuffix++;
@@ -370,7 +373,7 @@ int main(int argc, char *argv[])
 		double secondsElapsed = difftime(end, start);
 		double fps = frameCounter/secondsElapsed;
 
-		if (fps > goalFPS){
+		if (fps > (goalFPS-5)){
 			waitKey(100);
 		}
 
